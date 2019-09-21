da8de886de24fb9391f8beefebe30d8043706c6c
Agwenchez <agweraenock76@gmail.com>
2019-09-21 19:10:40 +0300

    dance studio website

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const request = require('request');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+app.use(express.json());
+
+// Bodyparser Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/subscribe', (req,res)=>{
+    res.send('ok')
+})
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, console.log(`Server started on ${PORT}`));
+
