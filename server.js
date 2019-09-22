8ade23a41785c236d214169b21218f15fdec028b
Agwenchez <agweraenock76@gmail.com>
2019-09-22 17:50:10 +0300

    bug fixes

--- server.js
+++ server.js
@@ -13,6 +13,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, console.log(`Server started on ${PORT}`));
